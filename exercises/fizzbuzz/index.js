ff5eabf2bc14c16bec85351b4404cb22149ae56b
WooSung Choi <johnwschoi@gmail.com>
2018-10-22 08:49:44 -0700

    fizzbuzz and maxchar complete

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,23 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // my attempt
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i)
+    }
+  }
+
+  return
+}
+
+fizzBuzz(5)
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // my first attempt
+  const hashMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (!hashMap[char]) {
+      hashMap[char] = 1;
+    } else {
+      hashMap[char]++;
+    }
+  }
+
+  for (let key in hashMap) {
+    if (maxNum < hashMap[key]) {
+      maxNum = hashMap[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+  // name hashMap as -> charMap
+}
 
 module.exports = maxChar;
