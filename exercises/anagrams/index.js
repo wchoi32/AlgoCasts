c2b7d48e9a7ab50b85a4aa7257487a98ecd85371
WooSung Choi <johnwschoi@gmail.com>
2018-10-25 08:25:52 -0700

    anagram and capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,66 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // My attempt
+  // const charAMap = {};
+  // const charBMap = {};
+
+  // const charA = stringA.replace(/[^\w]/g, '').toLowerCase();
+  // const charB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+  // if (charA.length !== charB.length) return false;
+
+  // for (let i = 0; i < charA.length; i++) {
+  //   if (!charAMap[charA[i]]) {
+  //     charAMap[charA[i]] = 1
+  //   } else {
+  //     charAMap[charA[i]]++
+  //   }
+
+  //   if (!charBMap[charB[i]]) {
+  //     charBMap[charB[i]] = 1
+  //   } else {
+  //     charBMap[charB[i]]++
+  //   }
+  // }
+
+  // for (let key in charAMap) {
+  //   if (charAMap[key] !== charBMap[key]) {
+  //     return false
+  //   }
+  // }
+
+  // return true
+
+  // Attempt Two
+  // const charAMap = buildCharMap(stringA);
+  // const charBMap = buildCharMap(stringB);
+
+  // if (Object.keys(charAMap).length !== Object.keys(charBMap).length) return false
+
+  // for (let key in charAMap) {
+  //   if (charAMap[key] !== charBMap[key]) return false;
+  // }
+
+  // return true;
+
+  const sortedStrA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  const sortedStrB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+  if (sortedStrA === sortedStrB) return true;
+
+  return false;
+}
+
+// function buildCharMap(str) {
+//   const charMap = {}
+
+//   for (char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char + 1] || 1
+//   }
+
+//   return charMap
+// }
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,22 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // My Attempt
+  // const capitalize = []
+
+  // str.split(' ').forEach(element => {
+  //   capitalize.push(element[0].toUpperCase() + element.slice(1))
+  // })
+
+  // return capitalize.join(' ')
+
+  // Cleaning it up SOL 2
+
+  return str.split(' ').map(element => {
+    return element[0].toUpperCase() + element.slice(1)
+  }).join(' ')
+
+}
 
 module.exports = capitalize;
