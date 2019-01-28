b4cdb46f5cf52e68d81b162482b3bcad0031ad48
John Choi <johnwschoi@gmail.com>
2019-01-27 19:30:19 -0800

    bst, events, level, tree, validate bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,29 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data <= this.data) {
+      if (this.left) return this.left.insert(data);
+      this.left = new Node(data);
+    }  else if (data > this.data) {
+      if (this.right) return this.right.insert(data);
+      this.right = new Node(data);
+    } 
+  }
+
+  contains(data) {
+    if(data === this.data) return this
+    if (data < this.data && this.left) return this.left.contains(data);
+    if (data > this.data && this.right) return this.right.contains(data);
+    return null
+  }
+}
 
 module.exports = Node;
